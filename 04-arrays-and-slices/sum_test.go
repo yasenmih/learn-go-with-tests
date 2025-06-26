75edfe813905a5152eec89d3849d149d341eb57b
Yasen Mihaylov <yasen.mihaylov@sap.com>
2025-06-26 13:47:34 +0300

    refactored code in 04-arrays-and-slices to work with slices

--- 04-arrays-and-slices/sum_test.go
+++ 04-arrays-and-slices/sum_test.go
@@ -3,10 +3,10 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3}
 
 	got := Sum(numbers)
-	want := 15
+	want := 6
 
 	if got != want {
 		t.Errorf("got %d want %d given %v", got, want, numbers)
